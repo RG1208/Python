da4a33ce8dea33bfccd1595954db326cffe77e80
Rachit Garg <rachitgarg1208@gmail.com>
2026-04-21 22:17:36 +0530

    feat: Chatbot with persistance(RAM)

---
+++ LangGraph_CampusX/09_Chatbot/01.py
@@ -0,0 +1,62 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+from langgraph.checkpoint.memory import MemorySaver
+
+load_dotenv()
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+llm = ChatOpenAI()
+
+
+def chat_node(state: ChatState):
+
+    # take user query from state
+    messages = state['messages']
+
+    # send to llm
+    response = llm.invoke(messages)
+
+    # response store state
+    return {'messages': [response]}
+
+checkpointer = MemorySaver()
+
+graph = StateGraph(ChatState)
+
+# add nodes
+graph.add_node('chat_node', chat_node)
+
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+# ------------------- Graph Image -------------------
+
+from IPython.display import Image, display
+
+png_data = chatbot.get_graph().draw_mermaid_png()
+
+with open("01.png", "wb") as f:
+    f.write(png_data)
+    print("Graph saved as '01.png'")
+
+display(Image(png_data))
+
+thread_id = "1"
+
+while True:
+    user_input = input("Type Here: ")
+
+    if user_input.strip().lower() in ["bye","exit","quit"]:
+        break
+
+    else:
+        config={"configurable":{"thread_id":thread_id}}
+        response = chatbot.invoke({"messages":[HumanMessage(content=user_input)]},config=config)
+        print("AI Message", response['messages'][-1].content)
