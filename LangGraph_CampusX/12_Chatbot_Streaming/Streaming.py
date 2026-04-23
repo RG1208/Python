48a7cbba849233ab11eb9ed29a79bda3400b8f9b
Rachit Garg <rachitgarg1208@gmail.com>
2026-04-23 22:58:48 +0530

    Streaming feature in chatbot

---
+++ LangGraph_CampusX/12_Chatbot_Streaming/Streaming.py
@@ -0,0 +1,57 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+from langgraph.checkpoint.memory import MemorySaver
+
+load_dotenv()
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+llm = ChatOpenAI()
+
+
+def chat_node(state: ChatState):
+
+    # take user query from state
+    messages = state['messages']
+
+    # send to llm
+    response = llm.invoke(messages)
+
+    # response store state
+    return {'messages': [response]}
+
+checkpointer = MemorySaver()
+
+graph = StateGraph(ChatState)
+
+# add nodes
+graph.add_node('chat_node', chat_node)
+
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+for message_chunk,metadata in chatbot.stream_invoke(
+    {"messages":[HumanMessage(content="What is LangGraph?")]},
+    config={"configurable":{"thread_id":"1"}},
+    stream_mode="messages"
+    ):
+    if message_chunk.content:
+        print(message_chunk.content,end=" ",flush=True)
+# ------------------- Graph Image -------------------
+
+from IPython.display import Image, display
+
+png_data = chatbot.get_graph().draw_mermaid_png()
+
+with open("01.png", "wb") as f:
+    f.write(png_data)
+    print("Graph saved as '01.png'")
+
+display(Image(png_data))
